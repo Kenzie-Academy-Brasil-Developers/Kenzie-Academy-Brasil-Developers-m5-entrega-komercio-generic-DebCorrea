f1d2634d1aa7970365fd543573940f14c4cdcc1f
Debora Correa <deboragomescorr@gmail.com>
2022-10-19 16:40:54 -0300

    feat: field is_active adjusted to default value True

--- products/models.py
+++ products/models.py
@@ -5,7 +5,7 @@ class Product(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     quantity = models.PositiveIntegerField()
-    is_active = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
 
     seller = models.ForeignKey(
         "accounts.Account", on_delete=models.CASCADE, related_name="products"
