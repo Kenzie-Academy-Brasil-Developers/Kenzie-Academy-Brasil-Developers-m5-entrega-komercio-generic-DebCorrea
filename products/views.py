81ef8508e0704db7399ab972d3cb719e74fd20ca
Debora Correa <deboragomescorr@gmail.com>
2022-10-21 08:43:17 -0300

    feat: pagination on list routes created

--- products/views.py
+++ products/views.py
@@ -1,6 +1,6 @@
-from _project.utils.mixins import SerializerByMethodMixin
 from rest_framework import generics
 from rest_framework.authentication import TokenAuthentication
+from utils.mixins import SerializerByMethodMixin
 
 from .models import Product
 from .permissions import IsProductOwnerOrReadOnly, IsSellerOrReadOnly
@@ -11,7 +11,7 @@ class ProductView(SerializerByMethodMixin, generics.ListCreateAPIView):
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsSellerOrReadOnly]
 
-    queryset = Product.objects
+    queryset = Product.objects.all()
     serializer_map = {
         "GET": GenericProductSerializer,
         "POST": DetailedProductSerializer,
@@ -25,5 +25,5 @@ class ProductDetailView(generics.RetrieveUpdateAPIView):
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsProductOwnerOrReadOnly]
 
-    queryset = Product.objects
+    queryset = Product.objects.all()
     serializer_class = DetailedProductSerializer
